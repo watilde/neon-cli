6ed3851c1bb18e9a4e47acdaee92522d61d8d933
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-01-04 23:19:49 +0100

    tmp

--- test/acceptance/help.js
+++ test/acceptance/help.js
@@ -1,11 +1,12 @@
 import { setup } from '../support/acceptance';
 
-process.stdout.write("AAAAAAAAAAAAAAA");
+process.stdout.write("AAAAAAAAAAAAAAA\n");
 describe('neon help', function() {
-  process.stdout.write("BBBBBBBBBBBBBBBBBBBB");
+  process.stdout.write("BBBBBBBBBBBBBBBBBBBB\n");
   setup();
-  process.stdout.write("CCCCCCCCCCCCCCCCCCC");
+  process.stdout.write("CCCCCCCCCCCCCCCCCCC\n");
   it('should print neon usage', function(done) {
+    process.stdout.write("DDDDDDDDDDDDDDDDDDDD\n");
     this.spawn(['help'])
         .wait('Usage:')
         .wait('neon new')
